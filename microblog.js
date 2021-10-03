0059cc00bb1f6c27c6fa6227e08fbbc00b172af9
Alex Earley <earleyalex@gmail.com>
2021-10-03 13:07:35 -0400

    Update microblog.js

--- microblog.js
+++ microblog.js
@@ -1,6 +1,6 @@
 (function () {
     let files = [
-        { index:2,path:'9-11-21.md'} ,
+        { index:0,path:'9-11-21.md'} ,
         { index:1,path:'10-3-21.md'}
     ];
     let converter = new showdown.Converter();
